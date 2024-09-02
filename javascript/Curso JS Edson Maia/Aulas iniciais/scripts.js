de0e7e07cc09075f762ce5c9d40fc9f992a1a9b1
Zayozin <zayocomercial@gmail.com>
2024-09-02 16:08:06 -0300

    aulas iniciais javascript

---
+++ javascript/Curso JS Edson Maia/Aulas iniciais/scripts.js
@@ -0,0 +1,39 @@
+// Comandos de saida 
+document.write("Olá mundo!")
+console.log("Olá mundo!")
+alert("Olá mundo!")
+
+// Variáveis e constantes
+var variavel = 0
+let variavel_com_let = 'Texto'
+const CONSTANTE = 3.14
+
+// Operadores e operações
+let n1 = 5
+let n2 = 3
+let n3 = '3'
+
+console.log(n1 + n2) // soma
+console.log(n1 - n2) // subtração
+console.log(n1 * n2) // multiplicação
+console.log(n1 / n2) // divisão
+console.log(n1 % n2) // resto
+
+console.log(n2 == n3) // igual a (apenas conteúdo igual)
+console.log(n2 === n3) // identico a (conteúdo + tipo iguais)
+console.log(n2 != n3) // diferente (apenas conteúdo)
+console.log(n2 !== n3) // não é identico (conteúdo + tipo)
+
+console.log(n1 > n2) // maior
+console.log(n1 < n2) // menor
+console.log(n1 >= n2) // maior ou igual
+console.log(n1 <= n2) // menor ou igual
+
+// Operadores lógicos
+let a = true
+let b = false
+
+console.log(a && b) // E
+console.log(a || b) // OU
+console.log(!a) // não
+console.log(!b) 
