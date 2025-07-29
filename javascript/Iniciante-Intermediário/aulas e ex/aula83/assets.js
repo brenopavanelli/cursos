312454848280632e9ce6bcc7435a1b8af9cac821
Breno Pavanelli dos Santos <zayocomercial@gmail.com>
2025-07-28 21:27:27 -0300

    aula83.js finalizada

---
+++ javascript/Iniciante-Intermediário/aulas e ex/aula83/assets.js
@@ -0,0 +1,15 @@
+function pularLinha()  {
+    console.log('');
+}
+
+function definirTitulo(titulo) {
+    console.log('====================================');
+    console.log(titulo.toUpperCase());
+    console.log('====================================');
+}
+
+function inserirLinha() {
+    console.log('-------------------------------------------------------------');
+}
+
+export { pularLinha, definirTitulo, inserirLinha };
