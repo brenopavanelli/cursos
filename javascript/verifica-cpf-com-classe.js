7e9a0e1081224d12196174967acf9666c68b4103
Breno Pavanelli dos Santos <138505121+brenopavanelli@users.noreply.github.com>
2025-08-12 16:36:25 -0300

    refactor: verifica-cpf-com-classe.js melhorando a estrutura do código
    Uso de métodos privados e estáticos

--- javascript/verifica-cpf-com-classe.js
+++ javascript/verifica-cpf-com-classe.js
@@ -1,36 +1,51 @@
 // recriando o ex de validar CPF usando classes
 
+/* 
+  Métodos públicos: podem ser acessados de qualquer lugar
+  Métodos privados: só podem ser acessados dentro da própria classe
+  Métodos estáticos: não podem ser acessados por instâncias da classe, mas sim pela classe em si, servem como métodos auxiliares (se não usar this dentro do método ou se for uma regra de negócio que não depende de uma instância)
+  Métodos estáticos privados: só podem ser acessados dentro da própria classe como pertence a classe em si. São regras de negócio exclusivas da classe
+*/
+
 class Cpf {
+  static CPF_LENGTH = 11;
+  static FACTOR_FIRST_DIGIT = 10;
+  static FACTOR_SECOND_DIGIT = 11;
+
+  static #cpfOnlyNumbers(array) {
+    return array.replace(/\D+/g, '');
+  }
+
+  static #computeDigit(array, factor) {
+    let cpfNumbers = Array.from(array, Number);
+    let total = cpfNumbers.reduce((acc, value) => {
+      if (factor > 1) {
+        acc += value * factor;
+        factor--;
+      }
+      return acc;
+    }, 0)
+
+    const rest = total % 11;
+    return rest < 2 ? 0 : 11 - rest;
+  }
+
   constructor(code) {
       this.code = code;
   }
+
+  get formated() {
+    return this.code.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+  }
+  
   get digits() {
-    function sliceToOnlyNumbers(array) {
-      return array.replace(/\D+/g, '');
-    }
-    let cpf = sliceToOnlyNumbers(this.code).slice(0, -2);
-
-    function sumTotal(array, startMultiplyScale = 10) {
-      let total = 0;
-      for (let i = 0; i < array.length; i++) {
-        total += array[i] * (startMultiplyScale - i);
-      }
-      return total;
-    }
-
-    function computeDigit(total) {
-      let digit = 11 - (total % 11) > 9 ? 0 : 11 - (total % 11);
-      cpf += digit;
-      return digit
-    }
-
-    let total = sumTotal(cpf);
-    let firstDigit = computeDigit(total)
-    total = sumTotal(cpf, 11);
-    let secondDigit = computeDigit(total);
+    let cpf = Cpf.#cpfOnlyNumbers(this.code).slice(0, -2);
+    let firstDigit = Cpf.#computeDigit(cpf, Cpf.FACTOR_FIRST_DIGIT);
+    let secondDigit = Cpf.#computeDigit(cpf + firstDigit, Cpf.FACTOR_SECOND_DIGIT);
 
     return `${firstDigit}${secondDigit}`;
   }
+  
   isValid() {
     if (this.code.length !== 14) return false;
     return this.digits === this.code.slice(-2);
@@ -44,11 +59,17 @@ const cpf3 = new Cpf('070.987.720-03');
 const cpf4 = new Cpf('025.456.789-01');
 const cpf5 = new Cpf('123.456.789-0058');
 const cpf6 = new Cpf('111.111.111-11');
+const cpf7 = new Cpf('11111111111');
 
-console.log(cpf1.isValid())
-console.log(cpf2.isValid())
-console.log(cpf3.isValid())
-console.log(cpf4.isValid())
-console.log(cpf5.isValid())
-console.log(cpf6.isValid())
+console.log(cpf1.isValid()); // false
+console.log(cpf2.isValid()); // true
+console.log(cpf3.isValid()); // true
+console.log(cpf4.isValid()); // false
+console.log(cpf5.isValid()); // false
+console.log(cpf6.isValid()); // true
+
+console.log(cpf7.digits);
+console.log(cpf7.formated);
+
+
 
