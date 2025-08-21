d08453f0b5b1d29f6d6ed35de777b0d851649a8d
Breno Pavanelli dos Santos <138505121+brenopavanelli@users.noreply.github.com>
2025-08-21 11:51:59 +0000

    aula117 finalizada: modulos

--- javascript/aulas/aula117/src/index.js
+++ javascript/aulas/aula117/src/index.js
@@ -1,11 +1,22 @@
-import {nome, sobrenome, falaNome} from './module1.js';
+import {nome as nome2, sobrenome2, falaNome} from './module1.js';
+import qualquerNome from './module1.js';
+import * as MeuModulo from './module2.js';
+import dadoPadrao from './module4.js';
 
-console.log(nome, sobrenome);
+console.log(dadoPadrao);
+
+console.log(qualquerNome(5,3)); // aqui o default é o que está sendo importado, pois é a saída padrão do módulo
+
+console.log(MeuModulo);
+
+console.log(nome2, sobrenome2);
 falaNome();
 
 function soma(a, b) {
   return a + b;
 }
+
+console.log(soma(5, 5))
 
 const objeto = {
   nome: 'Luiz',
---
+++ javascript/aulas/aula117/src/module3.js
@@ -0,0 +1,5 @@
+const comida = 'Pizza';
+const bebida = 'Água'; // não exportado, logo não pode ser importado tornando uma constante privada
+
+// também é possível exportar constantes e variáveis default
+export default comida;
