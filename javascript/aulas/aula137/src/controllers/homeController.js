a945471a929838340cd9e70509f30983f82a46af
brenopavanelli <brenopavanelli3@gmail.com>
2025-10-23 12:16:00 -0300

    feat: aula137 middlewares

---
+++ javascript/aulas/aula137/src/controllers/homeController.js
@@ -0,0 +1,7 @@
+exports.paginaInicial = (req, res) => {
+    res.render('index');
+}
+
+exports.envioDeFormulario = (req, res) => {
+    res.send('Formulario Recebido!')
+}
---
+++ javascript/aulas/aula137/src/middlewares/globalMiddleware.js
@@ -0,0 +1,4 @@
+module.exports = (req, res, next) => {
+    console.log();
+    console.log('middleware global acessado');
+}
