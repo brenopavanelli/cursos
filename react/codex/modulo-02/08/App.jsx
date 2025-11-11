4ab607791204d1f3dbc84420a7a433916932f5fe
brenopavanelli <brenopavanelli3@gmail.com>
2025-11-11 16:17:48 -0300

    modulo 01 e 02 react codex

---
+++ react/codex/modulo-02/08/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  const barcelonaImage = <img src={"https://i.imgur.com/qaQNzqi.png"}  alt="Barcelona" />;
+  const tokyoImage = <img src={"https://i.imgur.com/OAx1wzO.png"}  alt="Tokyo" />;
+  const ohioImage = <img src={"https://i.imgur.com/CxJjltu.png"}  alt="Ohio" />;
+
+  const imageGallery = [
+    <li>{barcelonaImage}</li>,
+    <li>{tokyoImage}</li>,
+    <li>{ohioImage}</li>
+  ]
+
+  return (
+    <ul>
+      {imageGallery}
+    </ul>
+  );
+}
