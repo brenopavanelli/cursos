457b07ed9f46fb50ca646c65c3c64d3496ecb384
brenopavanelli <brenopavanelli3@gmail.com>
2025-11-25 13:28:17 -0300

    feat: aula 18 codex react

---
+++ react/codex/modulo-04/18/App.jsx
@@ -0,0 +1,5 @@
+import ZoomableImage from "./ZoomableImage.js"
+
+export default function App() {
+  return <div><ZoomableImage imageURL={'https://i.imgur.com/ZIBbN09.jpg'} zoomFactor={10}/></div>;
+}
